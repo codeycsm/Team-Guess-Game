4fab89927d55269502fe0d350365a401bf8901bc
Codey <codeymcdaniel@gmail.com>
2018-11-02 10:43:13 -0600

    check for win workingand subtracting guesses when user guess is not in word to guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,36 @@
 let teams = [
-  "Cardinals",
-  "Falcons",
-  "Ravens",
-  "Bills",
-  "Panthers",
-  "Bears",
-  "Bengals",
-  "Browns",
-  "Cowboys",
-  "Broncos",
-  "Lions",
-  "Packers",
-  "Texans",
-  "Colts",
-  "Jaguars",
-  "Cheifs",
-  "Dolphins",
-  "Vikings",
-  "Patriots",
-  "Saints",
-  "Giants",
-  "Jets",
-  "Raiders",
-  "Steelers",
-  "Eagles",
-  "Chargers",
+  "cardinals",
+  "falcons",
+  "ravens",
+  "bills",
+  "panthers",
+  "bears",
+  "bengals",
+  "browns",
+  "cowboys",
+  "broncos",
+  "lions",
+  "packers",
+  "texans",
+  "colts",
+  "jaguars",
+  "cheifs",
+  "dolphins",
+  "vikings",
+  "patriots",
+  "saints",
+  "giants",
+  "jets",
+  "raiders",
+  "steelers",
+  "eagles",
+  "chargers",
   "49ers",
-  "Seahawks",
-  "Rams",
-  "Buccaneers",
-  "Titans",
-  "Redskins"
+  "seahawks",
+  "rams",
+  "buccaneers",
+  "titans",
+  "redskins"
 ];
 // letters user guessed
 let guessedLetters = [];
@@ -52,23 +52,31 @@ for (let i = 0; i < teamToGuess.length; i++) {
 document.getElementById("display").innerHTML = hideTeam.join("");
 // displays users guesses
 document.getElementById("lives").innerHTML = guesses;
+
 // event listener for when user presses a key
 document.addEventListener("keypress", function(pressed) {
-  //key user pressed
+  // key user pressed
   let keyPressed = pressed.key;
+  // check user guess to team to guess
+  let charExists = teamToGuess.indexOf(keyPressed) >= 0;
   // pushes key pressed into an array of pressed keys
   guessedLetters.push(keyPressed.toUpperCase());
-  // Checks if user guess is in the word to guess.
-  for (let i = 0; i < teamToGuess.length; i++) {
-    if (keyPressed.toUpperCase() === teamToGuess[i].toUpperCase()) {
-      hideTeam[i] = keyPressed.toUpperCase();
+  // Check if user guess is in team to guess string
+  if (charExists) {
+    // updates hidden array with char user correctly guessed
+    for (let i = 0; i < teamToGuess.length; i++) {
+      if (keyPressed.toUpperCase() === teamToGuess[i].toUpperCase()) {
+        hideTeam[i] = keyPressed.toUpperCase();
+      }
     }
+  } else {
+    guesses--;
   }
   // Checks if user has guessed the word correctly.
   if (hideTeam.join("").toUpperCase() === teamToGuess.toUpperCase()) {
-    console.log("You Win");
   }
   // Re display hidden team to guess and guesses left
   document.getElementById("display").innerHTML = hideTeam.join("");
   document.getElementById("guessedLetters").innerHTML = guessedLetters.join("");
+  document.getElementById("lives").innerHTML = guesses;
 });
