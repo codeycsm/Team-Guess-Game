84bce62c69e9bb42ec85c872fb7567b92e5d2257
Codey <codeymcdaniel@gmail.com>
2018-11-02 15:39:04 -0600

    moved functions into game object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,50 +32,94 @@ let teams = [
   "titans",
   "redskins"
 ];
+let keyPressed;
 // letters user guessed
 let guessedLetters = [];
 // users guess amount
 let guesses = 10;
+let wins = 0;
+let games = 0;
 let randomNum = Math.floor(Math.random() * 32);
 // Team chosen
 let teamToGuess = teams[randomNum];
 // array to hide and compare team and user guess
 let hideTeam = [];
 
-console.log("Team to guess " + teamToGuess);
-
-// hides team name and sets to '-'
-for (let i = 0; i < teamToGuess.length; i++) {
-  hideTeam.push("-");
-}
-// displays hidden array
-document.getElementById("display").innerHTML = hideTeam.join("");
-// displays users guesses
-document.getElementById("lives").innerHTML = guesses;
-// event listener for when user presses a key
-document.addEventListener("keypress", function(pressed) {
-  // key user pressed
-  let keyPressed = pressed.key;
-  // checks if keyPressed is in teamToGuess
-  let charExists = teamToGuess.indexOf(keyPressed) >= 0;
-  // pushes key pressed into an array of pressed keys
-  guessedLetters.push(keyPressed.toUpperCase());
-  // Check if user guess is in team to guess string
-  if (charExists) {
-    // updates hidden array with char user correctly guessed
+let game = {
+  updateHideTeam: function() {
     for (let i = 0; i < teamToGuess.length; i++) {
       if (keyPressed.toUpperCase() === teamToGuess[i].toUpperCase()) {
         hideTeam[i] = keyPressed.toUpperCase();
       }
     }
+  },
+  checkGuesses: function() {
+    if (guesses === 0) {
+      games++;
+      document.getElementById("wins").innerHTML = wins + " / " + games;
+      game.resetGame();
+    }
+  },
+  updateStats: function() {
+    document.getElementById("display").innerHTML = hideTeam.join("");
+    document.getElementById("guessedLetters").innerHTML = guessedLetters.join(
+      ""
+    );
+    document.getElementById("lives").innerHTML = guesses;
+  },
+  checkForWin: function() {
+    if (hideTeam.join("").toUpperCase() === teamToGuess.toUpperCase()) {
+      wins++;
+      games++;
+      document.getElementById("wins").innerHTML = wins + " / " + games;
+      game.resetGame();
+    }
+  },
+  resetGame: function() {
+    guesses = 10;
+    randomNum = Math.floor(Math.random() * 32);
+    teamToGuess = teams[randomNum];
+    hideTeam = [];
+    guessedLetters = [];
+    for (let i = 0; i < hideTeam.length; i++) {
+      hideTeam.pop("");
+    }
+    for (let i = 0; i < teamToGuess.length; i++) {
+      hideTeam.push("-");
+    }
+    document.getElementById("display").innerHTML = hideTeam.join("");
+    console.log(teamToGuess);
+  }
+};
+
+console.log("Team to guess " + teamToGuess);
+
+// hides team name and sets to '-'
+for (let i = 0; i < teamToGuess.length; i++) {
+  hideTeam.push(" _ ");
+}
+// displays hidden array
+document.getElementById("display").innerHTML = hideTeam.join("");
+// displays users guesses
+document.getElementById("lives").innerHTML = guesses;
+// displays users wins
+document.getElementById("wins").innerHTML = wins + " / " + games;
+// event listener for when user presses a key
+document.addEventListener("keypress", function(pressed) {
+  // key user pressed
+  keyPressed = pressed.key;
+  // pushes key pressed into an array of pressed keys
+  guessedLetters.push(keyPressed.toUpperCase());
+  // checks if keyPressed is in teamToGuess
+  let charExists = teamToGuess.indexOf(keyPressed) >= 0;
+  // Check if user guess is in team to guess string
+  if (charExists) {
+    game.updateHideTeam();
+    //updateHideTeam();
   } else {
     guesses--;
+    game.checkGuesses();
   }
-  // Checks if user has guessed the word correctly.
-  if (hideTeam.join("").toUpperCase() === teamToGuess.toUpperCase()) {
-  }
-  // Re display hidden team to guess and guesses left
-  document.getElementById("display").innerHTML = hideTeam.join("");
-  document.getElementById("guessedLetters").innerHTML = guessedLetters.join("");
-  document.getElementById("lives").innerHTML = guesses;
+  game.updateStats();
+  game.checkForWin();
 });
